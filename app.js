57867ca005371c2965c69e4f53fdf366beef1603
Charles Ivia <iviacharles@gmail.com>
2020-07-02 16:14:30 +0300

    Set up a simple express server

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./Key/fir-db-onserver-firebase-adminsdk-phdgz-dc27226277.json");
@@ -5,4 +8,20 @@ var serviceAccount = require("./Key/fir-db-onserver-firebase-adminsdk-phdgz-dc27
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fir-db-onserver.firebaseio.com",
+});
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://fir-db-onserver.firebaseio.com",
+});
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("index");
+});
+
+app.listen(port, () => {
+  console.log("App is listening to port");
 });
