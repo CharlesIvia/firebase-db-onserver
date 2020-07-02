964101c87f47ac61a916c57e4c62130e3b670903
Charles Ivia <iviacharles@gmail.com>
2020-07-02 16:26:23 +0300

    Event listener

--- Public/script.js
+++ Public/script.js
@@ -6,3 +6,17 @@ const age = document.getElementById("age");
 const addBtn = document.getElementById("addBtn");
 const updateBtn = document.getElementById("updateBtn");
 const removeBtn = document.getElementById("removeBtn");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  axios({
+    method: "post",
+    url: "/save",
+    data: {
+      user_id: userId.value,
+      first_name: firstName.value,
+      last_name: lastName.value,
+      age: age.value,
+    },
+  });
+});
